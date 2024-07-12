7e1764e24482cb69e002a7678d40e2816267fb3c
moshiurhossain <146842406+moshiurhossain@users.noreply.github.com>
2024-07-12 21:14:25 +0400

    removed error from contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect } from "react";
-import { Tooltip, initTWE,  Collapse,
-  Ripple, } from "tw-elements";
-import HeaderComponent from './components/Header';
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import { useEffect } from "react";
+// import { Tooltip, initTWE,  Collapse,
+//   Ripple, } from "tw-elements";
+import HeaderComponent from "../components/Header"
 
 
 function Contact() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
 //   useEffect(() => {
 //     initTWE({ Tooltip,Collapse, Ripple });
